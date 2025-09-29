da3100ad5a2a91ec0ce5ee63a10176473ce1081c
mo-seoudi <moseoudi@hotmail.com>
2025-09-29 15:59:14 +0400

    Delete server directory

--- server/src/middleware/requireOrg.js
+++
@@ -1,7 +0,0 @@
-// server/src/middleware/requireOrg.js
-export function requireOrg(req, res, next) {
-  if (!req.activeOrgId) {
-    return res.status(400).json({ message: "Select an organization first" });
-  }
-  next();
-}
